251f1fa24e841e1e30ed496a992e92427f0ed454
jakeberreth <jakeberreth@gmail.com>
2022-02-13 12:55:31 -0800

    Fixed bugs in calculateStringLength() function

--- seqAlign.c
+++ seqAlign.c
@@ -2,24 +2,101 @@
 #include <math.h>
 
 void appWrapper(char* inputSequences, char* flag, char* parameters) {
-    int i = 5, j = 5, k = 5, l = 5, o = 5, p = 5, size = 10000;
+    int i = 5, j = 5, k = 5, l = 5, o = 5, p = 5, size = 10000, score = 0;
     int* m = &i, *n = &j, *numMatch = &k, *numMis = &l, *numGap = &o, *numGapExt = &p;
     char *seq1 = (char*)malloc(sizeof(char) * size), *seq2 = (char*)malloc(sizeof(char) * size);
-    char* optSeq1 = (char*)malloc(sizeof(char) * size), *optSeq2 = (char*)malloc(sizeof(char) * size);
+    char *optSeq1 = (char*)malloc(sizeof(char) * size), *optSeq2 = (char*)malloc(sizeof(char) * size);
     int* penalties = getParameters(parameters);
+    char *seqName1 = (char*)malloc(sizeof(char) * 100), *seqName2 = (char*)malloc(sizeof(char) * 100);
 
     int match = *penalties, mismatch = *(penalties + 1), gapExt = *(penalties + 2), gap = *(penalties + 3);
 
-    calculateStringLength(inputSequences, m, n, seq1, seq2);
+    calculateStringLength(inputSequences, m, n, seq1, seq2, seqName1, seqName2);
 
     CELL (*table)[*m][*n] = malloc(sizeof *table);
-
+    
     initializeTable(*m, *n, table, gapExt, gap); 
+    
+    if(strcmp(flag, "0") == 0) {
+        score = fillTableGlobal(*m, *n, table, match, mismatch, gapExt, gap, seq1, seq2); 
+        retraceGlobal(*m, *n, table, match, mismatch, gapExt, gap, numMatch, numMis, numGapExt, numGap, seq1, seq2, optSeq1, optSeq2);
+    } else {
+        score = fillTableLocal(*m, *n, table, match, mismatch, gapExt, gap, seq1, seq2); 
+        retraceLocal(*m, *n, table, match, mismatch, gapExt, gap, numMatch, numMis, numGapExt, numGap, seq1, seq2, optSeq1, optSeq2);
+    }
 
-    int score = fillTable(*m, *n, table, match, mismatch, gapExt, gap, seq1, seq2); 
+    displayOptAlignment(optSeq1, optSeq2, score, match, mismatch, gap, gapExt, seq1, seq2, *numMatch, *numMis, *numGapExt, *numGap, seqName1, seqName2);
+}
 
-    retrace(*m, *n, table, match, mismatch, gapExt, gap, numMatch, numMis, numGapExt, numGap, seq1, seq2, optSeq1, optSeq2);
+void displayOptAlignment(char *optSeq1, char *optSeq2, int score, int match, int mismatch, int gap, int gapExt, char *seq1, char *seq2, int numMatch, int numMis, int numGapExt, int numGap, char *name1, char *name2) {
+    FILE *output;
+    output = fopen("report.txt", "w");
+    if(output == NULL) printf("The file could not be opened.");
 
+    fprintf(output, "INPUT:\n");
+    fprintf(output, "********\n\n");
+
+    fprintf(output, ">%s", name1);
+
+    int i = 0, j = 0, k = 0;
+    while(i < strlen(seq1)) {
+        while(j < 60) {
+            if(i < strlen(seq1)) {
+                fprintf(output, "%c", seq1[i]);
+            }
+            i++;
+            j++;
+        }
+        fprintf(output, "%c", '\n');
+        j = 0;
+    }
+
+    fprintf(output, "\n>%s", name2);
+
+    i = 0, j = 0;
+    while(i < strlen(seq2)) {
+        while(j < 60) {
+            if(i < strlen(seq2)) {
+                fprintf(output, "%c", seq2[i]);
+            }
+            i++;
+            j++;
+        }
+        fprintf(output, "%c", '\n');
+        j = 0;
+    }
+
+    fprintf(output, "\nOUTPUT:\n");
+    fprintf(output, "********\n\n");
+    fprintf(output, "Scores: match = %d mismatch = %d h = %d g = %d\n", match, mismatch, gapExt, gap);
+    fprintf(output, "Sequence 1 = \"%s\", length = %lu\n", name1, strlen(seq1));
+    fprintf(output, "Sequence 1 = \"%s\", length = %lu\n", name2, strlen(seq2));
+
+    i = 0, j = 0, k = 0;
+    printf("%d\n", strlen(optSeq1));
+    while(i < strlen(optSeq1)) {
+        fprintf(output, "S1  %d  ", i+1);
+        k = i;
+        while(j < 60) {
+            if(i < strlen(optSeq1)) {
+                fprintf(output, "%c", *(optSeq1 + i));
+            }
+            i++;
+            j++;
+        }
+        fprintf(output, "  %d\n", i+1);
+        fprintf(output, "S2  %d  ", i+1);
+        j = 0;
+        while(j < 60) {
+            if(i < strlen(optSeq2)) {
+                fprintf(output, "%c", optSeq2[k]);
+            }
+            i++;
+            k++;
+            j++;
+        }
+        fprintf(output, "  %d\n", k+1);
+    }
 }
 
 int getNext(int max, int s, int d, int i) {
@@ -28,49 +105,107 @@ int getNext(int max, int s, int d, int i) {
     else return 3;
 }
 
-void retrace(int m, int n, CELL (*table)[][n], int match, int mismatch, int gapExt, int gap, int *numMatch, int *numMis, int *numGapExt, int *numGap, char *seq1, char *seq2, char *optSeq1, char *optSeq2) {
+void retraceGlobal(int m, int n, CELL (*table)[][n], int match, int mismatch, int gapExt, int gap, int *numMatch, int *numMis, int *numGapExt, int *numGap, char *seq1, char *seq2, char *optSeq1, char *optSeq2) {
+    int i = m - 1, j = n - 1, substitution = 0, next = 0;
+
+    int maximum = max((*table)[i][j].Sscore, (*table)[i][j].Dscore, (*table)[i][j].Iscore);
+    next = getNext(maximum, (*table)[i][j].Sscore, (*table)[i][j].Dscore, (*table)[i][j].Iscore);
+
+    if(next == 1) {
+        substitution = sub(i, j, match, mismatch, seq1, seq2);
+        if(substitution == match) (*numMatch)++;
+        else (*numMis)++;
+    }
+    if(next == 2 || next == 3) {
+        (*numGap)++, (*numGapExt)++;
+    }
+    int k = 0;
+    while(i != 0 || j != 0) {
+        if(next == 1) {
+            char c = seq1[i];
+            //printf("%c", seq1[i]);
+            //printf("%d", k);
+            *(optSeq1 + k) = seq1[i-1];
+            *(optSeq2 + k) = seq2[j];
+            i -= 1, j -= 1;
+            if(i == 0 || j == 0) break;
+            maximum = max((*table)[i-1][j-1].Sscore, (*table)[i-1][j-1].Dscore, (*table)[i-1][j-1].Iscore);
+            next = getNext(maximum, (*table)[i-1][j-1].Sscore, (*table)[i-1][j-1].Dscore, (*table)[i-1][j-1].Iscore);
+            if(next == 2 || next == 3) (*numGap)++, (*numGapExt)++;
+            //printf("%s", optSeq1);
+        } else if(next == 2) {
+            *(optSeq1 + k) = seq1[i];
+            *(optSeq2 + k) = '_';
+            i -= 1;
+            maximum = max((*table)[i-1][j].Sscore + gapExt + gap, (*table)[i-1][j].Dscore + gap, (*table)[i-1][j].Iscore + gapExt + gap);
+            next = getNext(maximum, (*table)[i-1][j].Sscore + gapExt + gap, (*table)[i-1][j].Dscore + gap, (*table)[i-1][j].Iscore + gapExt + gap);
+            if(next == 2) (*numGap)++;
+            if(next == 3) (*numGap)++, (*numGapExt)++;
+        } else {
+            *(optSeq1 + k) = '_';
+            *(optSeq2 + k) = seq2[j];
+            j -= 1;
+            maximum = max((*table)[i][j-1].Sscore + gapExt + gap, (*table)[i][j-1].Dscore + gapExt + gap, (*table)[i][j-1].Iscore + gap);
+            next = getNext(maximum, (*table)[i][j-1].Sscore + gapExt + gap, (*table)[i][j-1].Dscore + gapExt + gap, (*table)[i][j-1].Iscore + gap);
+            if(next == 3) (*numGap)++;
+            if(next == 2) (*numGap)++, (*numGapExt)++;
+        }
+        k++;
+    }
+    // printf("%d %d %d %d\n", *numMatch, *numMis, *numGap, *numGapExt);
+    // printf("%d\n", (*numMatch)*match + (*numMis)*mismatch + (*numGap)*gap + (*numGapExt)*gapExt);
+    //printf("%s\n\n\n%s", optSeq1, optSeq2);
+    printf("%s", optSeq1);
+    //for(int i = 0; i<k-1; i++) printf("%c", *(optSeq2 + i));
+}
+
+void retraceLocal(int m, int n, CELL (*table)[][n], int match, int mismatch, int gapExt, int gap, int *numMatch, int *numMis, int *numGapExt, int *numGap, char *seq1, char *seq2, char *optSeq1, char *optSeq2) {
     int i = m - 1, j = n - 1, k = 0, substitution = 0, next = 0;
 
-    int maximum = max((*table)[m][n].Sscore, (*table)[m][n].Dscore, (*table)[m][n].Iscore);
-    next = getNext(maximum, (*table)[m][n].Sscore, (*table)[m][n].Dscore, (*table)[m][n].Iscore);
-    
+    int maximum = max((*table)[i][j].Sscore, (*table)[i][j].Dscore, (*table)[i][j].Iscore);
+    next = getNext(maximum, (*table)[i][j].Sscore, (*table)[i][j].Dscore, (*table)[i][j].Iscore);
+
+    if(next == 1) {
+        substitution = sub(i, j, match, mismatch, seq1, seq2);
+        if(substitution == match) (*numMatch)++;
+        else (*numMis)++;
+    }
+    if(next == 2 || next == 3) {
+        (*numGap)++, (*numGapExt)++;
+    }
     
     while(i != 0 || j != 0) {
-        printf("%d %d %d\n", next, i, j);
-        printf("%d\n", max((*table)[0][1].Iscore, (*table)[0][1].Iscore, (*table)[0][1].Iscore));
         if(next == 1) {
-            printf("hi\n");
-            substitution = sub(i, j, match, mismatch, seq1, seq2);
-            if(substitution == match) (*numMatch)++;
-            else (*numMis)++;
-            *(optSeq1 + k) = seq1[i], *(optSeq1 + k) = seq2[j];
+            *(optSeq1 + k) = *(seq1 + i), *(optSeq2 + k) = seq2[j];
             i -= 1, j -= 1;
-            printf("hi\n");
-            //it's right here: after subtracting, i=-1
-            printf("%d %d %d\n", (*table)[i-1][j-1].Sscore, (*table)[i-1][j-1].Dscore, (*table)[i-1][j-1].Iscore);
+            if(i == 0 || j == 0) break;
             maximum = max((*table)[i-1][j-1].Sscore, (*table)[i-1][j-1].Dscore, (*table)[i-1][j-1].Iscore);
-            printf("hi\n");
             next = getNext(maximum, (*table)[i-1][j-1].Sscore, (*table)[i-1][j-1].Dscore, (*table)[i-1][j-1].Iscore);
+            if(next == 2 || next == 3) (*numGap)++, (*numGapExt)++;
         } else if(next == 2) {
             *(optSeq1 + k) = seq1[i], *(optSeq2 + k) = '_';
             i -= 1;
             maximum = max((*table)[i-1][j].Sscore + gapExt + gap, (*table)[i-1][j].Dscore + gap, (*table)[i-1][j].Iscore + gapExt + gap);
             next = getNext(maximum, (*table)[i-1][j].Sscore + gapExt + gap, (*table)[i-1][j].Dscore + gap, (*table)[i-1][j].Iscore + gapExt + gap);
+            if(next == 2) (*numGap)++;
+            if(next == 3) (*numGap)++, (*numGapExt)++;
         } else {
             *(optSeq1 + k) = '_', *(optSeq2 + k) = seq2[j];
             j -= 1;
             maximum = max((*table)[i][j-1].Sscore + gapExt + gap, (*table)[i][j-1].Dscore + gapExt + gap, (*table)[i][j-1].Iscore + gap);
             next = getNext(maximum, (*table)[i][j-1].Sscore + gapExt + gap, (*table)[i][j-1].Dscore + gapExt + gap, (*table)[i][j-1].Iscore + gap);
+            if(next == 3) (*numGap)++;
+            if(next == 2) (*numGap)++, (*numGapExt)++;
         }
         k++;
-        printf("%d\n", next);
-        //printf("%d\n", k);
-        //printf("%d %d %d\n", next, i, j);
     }
-    printf("%d %d %d\n", next, i, j);
+    // printf("%d %d %d %d\n", *numMatch, *numMis, *numGap, *numGapExt);
+    // printf("%d\n", (*numMatch)*match + (*numMis)*mismatch + (*numGap)*gap + (*numGapExt)*gapExt);
+    //printf("%s", optSeq1);
+    //for(int i = 0; i<k-1; i++) printf("%c", *(optSeq2 + i));
 }
 
-int fillTable(int m, int n, CELL (*table)[][n], int match, int mismatch, int gapExt, int gap, char *seq1, char *seq2) {
+int fillTableLocal(int m, int n, CELL (*table)[][n], int match, int mismatch, int gapExt, int gap, char *seq1, char *seq2) {
     for(int i = 1; i < m; i++) {
         for(int j = 1; j < n; j++) {
             (*table)[i][j].Sscore = max((*table)[i-1][j-1].Sscore, (*table)[i-1][j-1].Dscore, (*table)[i-1][j-1].Iscore) + sub(i, j, match, mismatch, seq1, seq2);
@@ -79,9 +214,21 @@ int fillTable(int m, int n, CELL (*table)[][n], int match, int mismatch, int gap
         }
     }
 
-    int score = max((*table)[m][n].Sscore, (*table)[m][n].Dscore, (*table)[m][n].Iscore);
+    int score = max((*table)[m-1][n-1].Sscore, (*table)[m-1][n-1].Dscore, (*table)[m-1][n-1].Iscore);
 
-    printf("score = %d\n", score);
+    return score;
+}
+
+int fillTableGlobal(int m, int n, CELL (*table)[][n], int match, int mismatch, int gapExt, int gap, char *seq1, char *seq2) {
+    for(int i = 1; i < m; i++) {
+        for(int j = 1; j < n; j++) {
+            (*table)[i][j].Sscore = max((*table)[i-1][j-1].Sscore, (*table)[i-1][j-1].Dscore, (*table)[i-1][j-1].Iscore) + sub(i, j, match, mismatch, seq1, seq2);
+            (*table)[i][j].Dscore = max((*table)[i-1][j].Sscore + gapExt + gap, (*table)[i-1][j].Dscore + gap, (*table)[i-1][j].Iscore + gapExt + gap); 
+            (*table)[i][j].Iscore = max((*table)[i][j-1].Sscore + gapExt + gap, (*table)[i][j-1].Dscore + gapExt + gap, (*table)[i][j-1].Iscore + gap);
+        }
+    }
+
+    int score = max((*table)[m-1][n-1].Sscore, (*table)[m-1][n-1].Dscore, (*table)[m-1][n-1].Iscore);
 
     return score;
 }
@@ -99,6 +246,14 @@ int max(int a, int b, int c) {
     if(a >= b && a >= c) return a;
     else if(b >= a && b >= c) return b;
     else if(c >= a && c >= b) return c;
+
+    return 0;
+}
+
+int maxLocal(int a, int b, int c) {
+    if(a >= b && a >= c && a >= 0) return a;
+    else if(b >= a && b >= c && b >= 0) return b;
+    else if(c >= a && c >= b && c >= 0) return c;
 
     return 0;
 }
@@ -131,8 +286,6 @@ int* getParameters(char* parameters) {
     penalties[2] = gapExtInt;
     penalties[3] = gapInt;
 
-    printf("match = %d\nmismatch = %d\ngap extension = %d\ngap = %d\n", penalties[0], penalties[1], penalties[2], penalties[3]);
-
     return penalties;
 }
 
@@ -144,42 +297,53 @@ void initializeTable(int m, int n, CELL (*table)[][n], int h, int g) {
     for(int j = 1; j < n; j++) (*table)[0][j].Sscore = -10000, (*table)[0][j].Dscore = -10000, (*table)[0][j].Iscore = h + j*g;
 }
 
-void calculateStringLength(char* inputSequences, int* m, int* n, char *seq1, char *seq2) {
+void calculateStringLength(char* inputSequences, int* m, int* n, char *seq1, char *seq2, char *seqName1, char *seqName2) {
     char c;
-    int markerCount = 0, tempM = 0, tempN = 0, size = 10000, i = 0;
+    int markerCount = 0, tempM = 0, tempN = 0, size = 10000, i = 0, j = 0, k = 0;
     char tempStr[1];
 
     FILE *inSequences = fopen(inputSequences, "r");
 
     while((c = fgetc(inSequences)) != EOF) {
-        if(c == '>' && markerCount == 0) markerCount++;
-        if(c == '\n' && markerCount == 1) markerCount++;
+        if(c == '>' && markerCount == 0) {
+            markerCount++;
+        }
+        if(markerCount == 1) {
+            if(j != 0) {
+                *(seqName1 + (j-1)) = c;
+            }
+            j++;
+        }
+        if(c == '\n' && markerCount == 1) {
+            markerCount++;
+        }
         if(isBase(c) == 1 && markerCount == 2) {
             tempM++;
             *(seq1 + i) = c;
             i++;
         } 
-        if(c == '>' && markerCount == 2) markerCount++;
-        if(c == '\n' && markerCount == 3) {
-            *(seq1 + i) = '\0';
+        if(c == '>' && markerCount == 2) { 
             markerCount++;
-            i = 0;
+            j = 0; 
         }
-        
+        if(markerCount == 3) {
+            if(j != 0)
+                *(seqName2 + (j - 1)) = c;
+            j++;
+        }
+        if(c == '\n' && markerCount == 3) {
+            markerCount++;
+        }
         if(isBase(c) == 1 && markerCount == 4) {
             tempN++;
-            *(seq2 + i) = c;
-            i++;
+            *(seq2 + k) = c;
+            k++;
         }
-        
     }
-    *(seq2 + i) = '\0';
+    //*(seq2 + i) = '\0';
 
-    *m = tempM;
-    *n = tempN;
-
-    printf("m = %lu\n", strlen(seq1));
-    printf("n = %lu\n", strlen(seq2));
+    *m = tempM + 1;
+    *n = tempN + 1;
 }
 
 int isBase(char c) {
--- seqAlign.h
+++ seqAlign.h
@@ -10,11 +10,14 @@ typedef struct DP_cell {
 
 void appWrapper(char* inputSequences, char* flag, char* parameters);
 int* getParameters(char* parameters);
-void calculateStringLength(char* inputSequences, int* m, int* n, char *seq1, char *seq2);
-int fillTable(int m, int n, CELL (*table)[][n], int match, int mismatch, int gapExt, int gap, char *seq1, char *seq2);
+void calculateStringLength(char* inputSequences, int* m, int* n, char *seq1, char *seq2, char *seqName1, char *seqName2);
+int fillTableGlobal(int m, int n, CELL (*table)[][n], int match, int mismatch, int gapExt, int gap, char *seq1, char *seq2);
+int fillTableLocal(int m, int n, CELL (*table)[][n], int match, int mismatch, int gapExt, int gap, char *seq1, char *seq2);
 void initializeTable(int m, int n, CELL (*table)[][n], int h, int g);
-void retrace(int m, int n, CELL (*table)[][n], int match, int mismatch, int gapExt, int gap, int *numMatch, int *numMis, int *numGapExt, int *numGap, char *seq1, char *seq2, char *optSeq1, char *optSeq2);
+void retraceGlobal(int m, int n, CELL (*table)[][n], int match, int mismatch, int gapExt, int gap, int *numMatch, int *numMis, int *numGapExt, int *numGap, char *seq1, char *seq2, char *optSeq1, char *optSeq2);
+void retraceLocal(int m, int n, CELL (*table)[][n], int match, int mismatch, int gapExt, int gap, int *numMatch, int *numMis, int *numGapExt, int *numGap, char *seq1, char *seq2, char *optSeq1, char *optSeq2);
 int isBase(char c);
 int max(int a, int b, int c);
 int sub(int i, int j, int match, int mismatch, char *seq1, char *seq2);
 int getNext(int max, int s, int d, int i);
+void displayOptAlignment(char *optSeq1, char *optSeq2, int score, int match, int mismatch, int gap, int gapExt, char *seq1, char *seq2, int numMatch, int numMis, int numGapExt, int numGap, char *name1, char *name2);
